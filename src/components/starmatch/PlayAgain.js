738026e82acff96d5edffc67c54efe92ab14be08
unknown <khemrajsawant@gmail.com>
2020-04-05 14:50:39 +0530

    chages

--- src/components/starmatch/PlayAgain.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './styleStarMatch.css'
-const PlayAgain = (props) =>{
-
-    return (<div className = 'game-done'><button onClick={props.onClick}>PlayAgain</button></div>)
-}
-
-export default PlayAgain
